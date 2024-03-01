12f432b5b93207974db8d5bd6876a9075e9b1a67
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-03-01 20:38:20 +0900

    feat: 팝업스토어 repository 추가

---
+++ redis-cache/src/main/java/com/example/rediscache/repository/PopUpStoreInfoRepository.java
@@ -0,0 +1,9 @@
+package com.example.rediscache.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.rediscache.domain.PopUpStoreInfo;
+
+public interface PopUpStoreInfoRepository extends CrudRepository<PopUpStoreInfo, Long> {
+
+}
