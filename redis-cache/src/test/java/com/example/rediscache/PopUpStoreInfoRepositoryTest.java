5c96b198732569004932eb630bb9cad44c468f98
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-03-02 22:43:43 +0900

    test: 데이터 추가 및 얻기 기능 추가

---
+++ redis-cache/src/test/java/com/example/rediscache/PopUpStoreInfoRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.rediscache;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.rediscache.domain.PopUpStoreInfo;
+import com.example.rediscache.repository.PopUpStoreInfoRepository;
+
+@SpringBootTest
+public class PopUpStoreInfoRepositoryTest {
+
+	@Autowired
+	private PopUpStoreInfoRepository repo;
+
+	@Test
+	void saveTest() {
+		PopUpStoreInfo store = new PopUpStoreInfo(0L, "경희대학교", "용인", "경기도 용인시 어쩌구 저쩌구", "경희대인스타", "경희대닷컴", 0,
+			LocalDate.now(), null, LocalTime.now(), null, true);
+
+		repo.save(store);
+		repo.findById(store.getId());
+		repo.count();
+		// repo.delete(store);
+	}
+
+	@Test
+	void getTest() {
+		System.out.println("getTest");
+		repo.findById(0L).ifPresent(System.out::println);
+		// repo.findAll().forEach(System.out::println);
+	}
+}
