6279375f9df3af5b599e275b3c9077b52ce50951
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-03-03 17:03:57 +0900

    style: 변수 라인 변경

--- redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
+++ redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
@@ -4,16 +4,30 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash(value = "store", timeToLive = 300)
 public class PopUpStoreInfo {
 	@Id
 	private Long id;
-	private String name, region, address, snsUrl, webUrl;
+	private String name;
+	private String region;
+	private String address;
+	private String snsUrl;
+	private String webUrl;
 	private int ticketPrice;
-	private LocalDate startDate, endDate;
-	private LocalTime startTime, endTime;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private LocalTime startTime;
+	private LocalTime endTime;
 	private boolean parkingAvailability;
 }
