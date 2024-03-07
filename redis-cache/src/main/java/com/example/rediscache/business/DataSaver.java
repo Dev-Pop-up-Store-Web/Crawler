6909e5fa0fcd857b10591bba5292c76c04a7d812
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-03-07 17:17:15 +0900

    feat: redis에 매일 정각 데이터 저장 기능 추가

---
+++ redis-cache/src/main/java/com/example/rediscache/business/DataSaver.java
@@ -0,0 +1,25 @@
+package com.example.rediscache.business;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.example.rediscache.domain.PopUpStoreInfo;
+import com.example.rediscache.repository.redis.PopUpStoreRedisRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+class DataSaver {
+	private final PopUpStoreRedisRepository popUpStoreRedisRepository;
+	private final DataFetcher dataFetcher;
+
+	@Scheduled(cron = "0 0 * * *") // 매일 정각에 한 번 실행
+	public void saveData() {
+		var newData = dataFetcher.findDataByDateField();
+		popUpStoreRedisRepository.saveAll(newData);
+	}
+}
