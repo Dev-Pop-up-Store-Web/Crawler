fa57d3e55413a72cdb8a52a946f5a2b924f21d63
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-03-07 17:11:02 +0900

    refactor: 인스턴스 변수 변경

--- redis-cache/src/test/java/com/example/rediscache/PopUpStoreRedisRepositoryTest.java
+++ redis-cache/src/test/java/com/example/rediscache/PopUpStoreRedisRepositoryTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.rediscache.domain.PopUpStoreInfo;
-import com.example.rediscache.repository.PopUpStoreRedisRepository;
+import com.example.rediscache.repository.redis.PopUpStoreRedisRepository;
 
 @SpringBootTest
 public class PopUpStoreRedisRepositoryTest {
@@ -19,7 +19,7 @@ public class PopUpStoreRedisRepositoryTest {
 	@Test
 	void saveTest() {
 		PopUpStoreInfo store = new PopUpStoreInfo("1", "경희대학교", "용인", "경기도 용인시 어쩌구 저쩌구", "경희대인스타", "경희대닷컴", 0,
-			LocalDate.now(), null, LocalTime.now(), null, true);
+			LocalDate.now(), null, LocalDate.now(), LocalTime.now(), null, true);
 
 		repo.save(store);
 		repo.findById(store.getId());
