f4bbbdc47190f1317152f02120d7142a08121c47
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-03-14 21:32:04 +0900

    refactor: domain 클래스 통일

--- redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
+++ redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
@@ -1,9 +1,12 @@
 package com.example.rediscache.domain;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.redis.core.RedisHash;
 
@@ -17,20 +20,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @RedisHash(value = "store", timeToLive = 86400)
-@Document
+@Document(collection = "infos")
 public class PopUpStoreInfo {
 	@Id
 	private String id;
 	private String name;
-	private String region;
-	private String address;
-	private String snsUrl;
-	private String webUrl;
-	private int ticketPrice;
 	private LocalDate startDate;
 	private LocalDate endDate;
-	private LocalDate updatedDate;
-	private LocalTime startTime;
-	private LocalTime endTime;
+	private List<String> openTimes;
+	private String region;
+	private String address;
+	private int ticketPrice;
+	private String snsUrl;
+	private String webUrl;
 	private boolean parkingAvailability;
+
+	@CreatedDate
+	private LocalDateTime createdDate;
+	@LastModifiedDate
+	private LocalDateTime modifiedDate;
+	private List<String> urls;
+
+	public PopUpStoreInfo(String id, String name, LocalDate startDate, LocalDate endDate, List<String> openTimes,
+		String region, String address, int ticketPrice, String snsUrl, String webUrl, boolean parkingAvailability,
+		List<String> urls) {
+		this.id = id;
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.openTimes = openTimes;
+		this.region = region;
+		this.address = address;
+		this.ticketPrice = ticketPrice;
+		this.snsUrl = snsUrl;
+		this.webUrl = webUrl;
+		this.parkingAvailability = parkingAvailability;
+		this.urls = urls;
+	}
 }
