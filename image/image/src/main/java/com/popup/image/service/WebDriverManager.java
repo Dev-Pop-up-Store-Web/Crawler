c889a8e9af51669b2707aa358bdf5b6b2758f915
seolroh <seven800@naver.com>
2024-03-16 01:06:40 +0900

    feat: add mongoconfig function

--- image/image/src/main/java/com/popup/image/service/WebDriverManager.java
+++ image/image/src/main/java/com/popup/image/service/WebDriverManager.java
@@ -1,44 +1,62 @@
 package com.popup.image.service;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebDriverManager {
 	private static WebDriver driver;
-	private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
-	// chromedriver 설치 경로 입력
-	private static final String WEB_DRIVER_PATH = "C:\\Users\\seol\\Downloads\\chromedriver\\chromedriver.exe";
+
+	public static WebDriver initChromeDriver() {
+		return setChromeDriver();
+	}
+
+	// private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+	// // chromedriver 설치 경로 입력
+	// private static final String WEB_DRIVER_PATH = "C:\\Users\\seol\\Downloads\\chromedriver\\chromedriver.exe";
 
 	private WebDriverManager() {
 		// private 생성자로 외부에서 인스턴스 생성을 막음
 	}
+	private static WebDriver setChromeDriver() {
+		try{
+			String osName = System.getProperty("os.name").toLowerCase();
+			ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder();
 
-	public static WebDriver initChromeDriver() {
-		setDriverOptions();
-		driver = new ChromeDriver(getChromeOptions());
-		return driver;
-	}
+			// mac
+			if (osName.contains("mac"))
+				serviceBuilder.usingDriverExecutable(new File("/usr/local/bin/chromedriver"));
 
-	private static void setDriverOptions() {
-		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-	}
+				// raspberryPi
+			else if (osName.contains("linux") && osName.contains("arm"))
+				serviceBuilder.usingDriverExecutable(new File("/usr/local/bin/chromedriver"));
 
-	private static ChromeOptions getChromeOptions() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
-		return options;
-	}
+				// windows
+			else if (osName.contains("win"))
+				serviceBuilder.usingDriverExecutable(new File("C:\\path\\to\\chromedriver.exe"));
 
-	public static WebDriver getDriver() {
-		return driver;
-	}
+				// docker container
+			else if (osName.contains("linux"))
+				serviceBuilder.usingDriverExecutable(new File("/usr/local/bin/chromedriver"));
 
-	public static void quitDriver() {
-		if (driver != null) {
-			driver.quit();
+			ChromeDriverService service = serviceBuilder.usingPort(9387).build(); // -> 포트 다르게 설정
+			service.start();
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless"); // headless 모드 활성화
+			options.addArguments("--no-sandbox"); // no-sandbox 옵션 추가
+			// options.addArguments("--disable-dev-shm-usage"); //  unknown error: session deleted because of page crash
+			// options.addArguments("--remote-debugging-pipe");
+
+			return new ChromeDriver(service, options);
+		}
+		catch(Exception ex){
+			return null;
 		}
 	}
 }
