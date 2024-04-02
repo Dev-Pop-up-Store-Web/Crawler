b574c1c87de7e528bef53f608584bb49353cb7de
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-04-03 00:38:51 +0900

    feat: redisTemplate 추가

--- redis-cache/src/main/java/com/example/rediscache/config/RedisConfig.java
+++ redis-cache/src/main/java/com/example/rediscache/config/RedisConfig.java
@@ -5,9 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisRepositories(basePackages = "com.example.rediscache.repository.redis")
+import com.example.rediscache.domain.PopUpStoreInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 @Configuration
 public class RedisConfig {
 
@@ -20,5 +25,17 @@ public class RedisConfig {
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+
+	@Bean
+	public RedisTemplate<String, PopUpStoreInfo> redisTemplate() {
+		RedisTemplate<String, PopUpStoreInfo> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(serializer);
+		return redisTemplate;
 	}
 }
