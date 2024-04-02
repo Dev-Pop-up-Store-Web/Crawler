bc0649b7125bc6cb951034e10de734683898f02c
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-04-03 00:42:32 +0900

    refactor: 데이터 유지시간 설정 및 추가

--- redis-cache/src/main/java/com/example/rediscache/business/DataSaver.java
+++ redis-cache/src/main/java/com/example/rediscache/business/DataSaver.java
@@ -1,22 +1,35 @@
 package com.example.rediscache.business;
 
+import java.time.Duration;
+import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
 
-import com.example.rediscache.repository.redis.PopUpStoreRedisRepository;
+import com.example.rediscache.domain.PopUpStoreInfo;
 
 import jakarta.annotation.PostConstruct;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@Component
-@AllArgsConstructor
+@Service
+@RequiredArgsConstructor
 class DataSaver {
-	private final PopUpStoreRedisRepository popUpStoreRedisRepository;
+	private final RedisTemplate<String, PopUpStoreInfo> redisTemplate;
 	private final DataFetcher dataFetcher;
+
+	@Value("${redis.cache.duration.days}")
+	private int redisCacheDurationDays;
 
 	@PostConstruct
 	public void saveData() {
-		var newData = dataFetcher.findDataByModifiedDate();
-		popUpStoreRedisRepository.saveAll(newData);
+		List<PopUpStoreInfo> newDataList = dataFetcher.findDataByModifiedDate();
+		newDataList.forEach(this::saveDataToRedis);
+	}
+
+	private void saveDataToRedis(PopUpStoreInfo popUpStoreInfo) {
+		redisTemplate.opsForValue()
+			.set(RedisKeyGenerator.generateKey(popUpStoreInfo), popUpStoreInfo,
+				Duration.ofDays(redisCacheDurationDays));
 	}
 }
