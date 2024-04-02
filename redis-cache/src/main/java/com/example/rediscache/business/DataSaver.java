4d4d8e37961db3ac2a0dd23384c7b7fad239c408
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-04-02 18:54:33 +0900

    refactor: cron 제거

--- redis-cache/src/main/java/com/example/rediscache/business/DataSaver.java
+++ redis-cache/src/main/java/com/example/rediscache/business/DataSaver.java
@@ -1,14 +1,11 @@
 package com.example.rediscache.business;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.example.rediscache.domain.PopUpStoreInfo;
 import com.example.rediscache.repository.redis.PopUpStoreRedisRepository;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 
 @Component
@@ -17,8 +14,7 @@ class DataSaver {
 	private final PopUpStoreRedisRepository popUpStoreRedisRepository;
 	private final DataFetcher dataFetcher;
 
-	// @Scheduled(cron = "0 0 * * * *") // 매일 정각에 한 번 실행
-	@Scheduled(cron = "10 * * * * *")
+	@PostConstruct
 	public void saveData() {
 		var newData = dataFetcher.findDataByModifiedDate();
 		popUpStoreRedisRepository.saveAll(newData);
