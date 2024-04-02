eb64c598ef00d5f88856d09e5cf4c7cb39de4e45
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-04-02 18:51:29 +0900

    refactor: 도메인 클래스 형식 통일

--- redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
+++ redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
@@ -22,6 +22,7 @@ import lombok.NoArgsConstructor;
 @RedisHash(value = "store", timeToLive = 86400)
 @Document(collection = "infos")
 public class PopUpStoreInfo {
+
 	@Id
 	private String id;
 	private String name;
@@ -34,27 +35,10 @@ public class PopUpStoreInfo {
 	private String snsUrl;
 	private String webUrl;
 	private boolean parkingAvailability;
+	private List<String> urls;
 
 	@CreatedDate
 	private LocalDateTime createdDate;
 	@LastModifiedDate
 	private LocalDateTime modifiedDate;
-	private List<String> urls;
-
-	public PopUpStoreInfo(String id, String name, LocalDate startDate, LocalDate endDate, List<String> openTimes,
-		String region, String address, int ticketPrice, String snsUrl, String webUrl, boolean parkingAvailability,
-		List<String> urls) {
-		this.id = id;
-		this.name = name;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.openTimes = openTimes;
-		this.region = region;
-		this.address = address;
-		this.ticketPrice = ticketPrice;
-		this.snsUrl = snsUrl;
-		this.webUrl = webUrl;
-		this.parkingAvailability = parkingAvailability;
-		this.urls = urls;
-	}
 }
