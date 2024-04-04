d90420308d166ccd316c97b504745d61940f6db0
seolroh <seven800@naver.com>
2024-04-04 23:30:18 +0900

    refactor: change Injection Dependency

--- image/image/src/main/java/com/popup/image/ImageApplication.java
+++ image/image/src/main/java/com/popup/image/ImageApplication.java
@@ -1,11 +1,8 @@
 package com.popup.image;
 
-import java.io.IOException;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 import com.popup.image.controller.CrawlerController;
@@ -16,13 +13,10 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @SpringBootApplication
 public class ImageApplication {
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(ImageApplication.class, args);
-	}
-	@Bean
-	public CommandLineRunner commandLineRunner(CrawlerController crawlerController) {
-		return args -> {
-			crawlerController.startCrawling();
-		};
+	public static void main(String[] args) {
+		//SpringApplication.run(ImageApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ImageApplication.class, args);
+		CrawlerController crawlerController = context.getBean(CrawlerController.class);
+		crawlerController.startCrawling();
 	}
 }
