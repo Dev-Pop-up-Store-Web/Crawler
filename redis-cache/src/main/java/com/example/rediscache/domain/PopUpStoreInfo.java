8d5da8b7e8e1de2422b7bb976739933dbe43df17
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-04-15 18:52:27 +0900

    style: 필요없는 라이브러리 호출 제거

--- redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
+++ redis-cache/src/main/java/com/example/rediscache/domain/PopUpStoreInfo.java
@@ -1,14 +1,12 @@
 package com.example.rediscache.domain;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.redis.core.RedisHash;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
